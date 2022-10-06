be266ffcfa17951d6e2e55c50577ba961cb4f6f1
Mike Orsow <mike.orsow@gmail.com>
2022-10-06 16:44:29 -0700

    Add Player Factory and ability to add a move

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-const gameboard = ['x','o','x','o','x','o','x','o','x']
+// Gameboard Module
+const gameboard = (() => {
+  let _boardMoves = [];
+  const _gameboardDivs = document.querySelectorAll('.gameboard div');
+  const display = () =>
+    _boardMoves.map(
+      (playerMove, i) => (_gameboardDivs[i].textContent = playerMove)
+    );
+  const addMove = (index, value) => {
+    _boardMoves[index] = value;
+    display();
+  };
+  return {
+    display,
+    addMove,
+  };
+})();
 
-const gameboardDivs = document.querySelectorAll('.gameboard div');
+// Player Factory
+const player = (name, moveType) => {
+  const getName = () => name;
+  const getMoveType = () => moveType;
+  return {
+    getName,
+    getMoveType,
+  };
+};
 
-gameboard.map((playerMove, i) => gameboardDivs[i].textContent = playerMove);
+gameboard.display();
