cfc67b1bfdd36e878bf2bb962f37ad7572a13184
Mike Orsow <mike.orsow@gmail.com>
2022-10-06 22:22:15 -0700

    Add a reset button

--- script.js
+++ script.js
@@ -2,27 +2,46 @@
 const gameboard = (() => {
   let _boardMoves = [];
   const _gameboardDivs = document.querySelectorAll('.gameboard div');
-  const display = () =>
-    _boardMoves.map(
-      (playerMove, i) => (_gameboardDivs[i].textContent = playerMove)
+
+  // Clicking on a tic tac toe square triggers a 'move'
+  const addClickListeners = (elements) => {
+    elements.forEach((element) =>
+      element.addEventListener(
+        'click',
+        (e) => {
+          gameFlow.move(e.target.dataset.index);
+        },
+        { once: true }
+      )
     );
+  };
+
+  // Renders the current game moves onto the board
+  const render = () => {
+    Array.from(_gameboardDivs).map((div, index) => {
+      div.textContent = _boardMoves[index];
+    });
+  };
+
   const addMove = (index, value) => {
     _boardMoves[index] = value;
-    display();
+    render();
   };
-  // Click listener to turn board clicks into moves
-  _gameboardDivs.forEach((square) =>
-    square.addEventListener(
-      'click',
-      (e) => {
-        gameFlow.move(e.target.dataset.index);
-      },
-      { once: true }
-    )
-  );
+
+  const reset = () => {
+    _boardMoves = [];
+    render();
+    addClickListeners(_gameboardDivs);
+  };
+
+  // Clicking the reset button clears the board
+  const resetButton = document.querySelector('.reset-button');
+  resetButton.addEventListener('click', reset);
+
   return {
-    display,
+    render,
     addMove,
+    reset,
   };
 })();
 
@@ -30,42 +49,41 @@ const gameboard = (() => {
 const player = (name, moveType) => {
   const getName = () => name;
   const getMoveType = () => moveType;
-  //   const move = (boardIndex) => {
-  //     gameboard.addMove(boardIndex, moveType);
-  //     gameFlow.nextMove;
-  //   };
   return {
     getName,
     getMoveType,
   };
 };
 
-// Players
-
 // Game Flow Module
 const gameFlow = (() => {
   // Players
   const player1 = player('James', 'x');
   const player2 = player('Laura', 'o');
+
   // Move Count
   let moveCount = 0;
   const nextMove = () => {
     moveCount++;
   };
+
   const getCurrentMoveType = () => {
     if (moveCount % 2 === 0) {
       return player1.getMoveType();
     }
     return player2.getMoveType();
   };
+
   const move = (boardIndex) => {
     gameboard.addMove(boardIndex, getCurrentMoveType());
     nextMove();
   };
+
   return {
     nextMove,
     move,
   };
 })();
 
-gameboard.display();
+gameboard.reset();
+gameboard.render();
